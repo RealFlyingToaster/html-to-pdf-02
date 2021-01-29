9b368a166a068814b24364da849dc371b7eda19a
Jack Eastman <jeastman@eightfoldway.com>
2021-01-28 16:05:44 -0800

    First upload

---
+++ handlers/trigger/index.js
@@ -0,0 +1,64 @@
+/**
+ * Trigger lambda function:
+ * - respond on API Gateway
+ * - accept event data
+ * - create a job token
+ * - trigger a work lambda to do the work
+ * - return the job token
+ */
+const { v4: uuidv4 } = require('uuid'),
+    AWS = require('aws-sdk');
+
+// set the region
+AWS.config.update({ region: process.env.REGION });
+
+let response,
+    sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+
+exports.lambdaHandler = async (event, context) => {
+    try {
+        const sqsQueueUrl = process.env.SQS_QUEUE,
+            jobToken = uuidv4();
+
+        console.log('trigger: ' + JSON.stringify(event));
+        console.log('trigger: posting ' + jobToken + ' to ' + sqsQueueUrl);
+
+        let params = {
+            MessageAttributes: {
+                "Job": {
+                    DataType: "String",
+                    StringValue: jobToken
+                }
+            },
+            MessageBody: event.body || "{}",
+            QueueUrl: sqsQueueUrl
+        };
+
+        await sqs.sendMessage(params).promise()
+            .then((data) => {
+                console.log('trigger: succcess: ' + data.MessageId);
+                response = {
+                    'statusCode': 200,
+                    'body': JSON.stringify({
+                        event: event,
+                        job: jobToken,
+                        sqsQueue: sqsQueueUrl,
+                        messageId: data.messageId
+                    })
+                };
+            }).catch((err) => {
+                console.error('trigger: ' + err);
+                throw (err);
+            });
+    } catch (err) {
+        console.log(err);
+        response = {
+            'statusCode': 500,
+            'body': JSON.stringify({
+                error: err
+            })
+        }
+    }
+
+    return response;
+};
